e2619a2c3636555e8135483c6ed291d132033951
glassbox71 <glassbox71@naver.com>
2025-05-30 10:59:39 +0900

    redirects

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import Layout from './components/Layout';
+import About from './pages/About';
+import Skill from './pages/Skill';
+import Portfolio from './pages/Portfolio';
+import Todolist from './pages/Todolist';
+import Contact from './pages/Contact';
+import "./App.scss"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Main />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skill" element={<Skill />} />
+          <Route path="/porfolio" element={<Portfolio />} />
+          <Route path="/todolist" element={<Todolist />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
